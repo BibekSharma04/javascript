07707e353a8cac40c5277018f55bedae62f89c23
BibekSharma04 <bibekspkt125@gmail.com>
2025-06-29 14:54:50 +0545

    Update test.js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("Hello Bibek");
+// 7 type of primitive data type
+//nnssbbu  number null symbol string boolean bigInt undefined 
+
+const Student = {
+     fullname:"Bibek Sharma",
+     age:19,
+     gpa:3.65,
+     isFollow:true
+};
+ Student["age"]=Student["age"]+1;
+ console.log(Student["age"]); 
